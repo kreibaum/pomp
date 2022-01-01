a0edcdac648e496e17377adfd93d1f6d40df114b
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2022-01-01 23:21:14 +0100

    Make ui a bit more pretty.

--- src/frontend/Page/Pomp.elm
+++ src/frontend/Page/Pomp.elm
@@ -1,6 +1,7 @@
 module Page.Pomp exposing (view)
 
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import LiveData exposing (..)
 
@@ -9,20 +10,64 @@ view : PompLiveState -> Html PompRemoteEvent
 view model =
     div []
         [ viewMyInventory model.myInventory
-        , div [] [ text " --- " ]
+        , viewMarketplace
         , viewOthers model.others
         ]
 
 
 viewMyInventory : PompMyInventory -> Html PompRemoteEvent
 viewMyInventory inventory =
-    div []
-        [ div [] [ text ("You have " ++ String.fromInt inventory.energy ++ " energy.") ]
-        , button [ onClick (Buy Fire) ] [ text ("Buy Fire (" ++ String.fromInt inventory.fire ++ ")") ]
-        , button [ onClick (Buy Plant) ] [ text ("Buy Plant (" ++ String.fromInt inventory.plant ++ ")") ]
-        , button [ onClick (Buy Water) ] [ text ("Buy Water (" ++ String.fromInt inventory.water ++ ")") ]
-        , button [ onClick (Buy Earth) ] [ text ("Buy Earth (" ++ String.fromInt inventory.earth ++ ")") ]
-        , button [ onClick (Buy Chaos) ] [ text ("Buy Chaos (" ++ String.fromInt inventory.chaos ++ ")") ]
+    div [ class "m-1 bg-gray-100 p-1 sm:p-2" ]
+        [ div [ class "flex flex-row sm:space-x-1" ]
+            [ div [ class "basis-1/4 text-center p-1" ] [ text "0 Points" ]
+            , div [ class "basis-2/4 text-center p-1 font-bold" ] [ text inventory.name ]
+            , div [ class "basis-1/4 text-center p-1" ] [ text (String.fromInt inventory.energy ++ " Energy") ]
+            ]
+        , div [ class "flex flex-row sm:space-x-1" ]
+            [ button [ onClick (Buy Fire), class "basis-1/5 text-center p-1 bg-red-200 hover:bg-red-300 active:bg-red-400 border-red-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt inventory.fire ++ ") Fire")
+                ]
+            , button [ onClick (Buy Plant), class "basis-1/5 text-center p-1 bg-green-200 hover:bg-green-300 active:bg-green-400 border-green-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt inventory.plant ++ ") Plant")
+                ]
+            , button [ onClick (Buy Water), class "basis-1/5 text-center p-1 bg-blue-200 hover:bg-blue-300 active:bg-blue-400 border-blue-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt inventory.water ++ ") Water")
+                ]
+            , button [ onClick (Buy Earth), class "basis-1/5 text-center p-1 bg-amber-200 hover:bg-amber-300 active:bg-amber-400 border-amber-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt inventory.earth ++ ") Earth")
+                ]
+            , button [ onClick (Buy Chaos), class "basis-1/5 text-center p-1 bg-purple-200 hover:bg-purple-300 active:bg-purple-400 border-purple-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt inventory.chaos ++ ") Chaos")
+                ]
+            ]
+        ]
+
+
+viewMarketplace : Html a
+viewMarketplace =
+    div [ class "m-1 bg-gray-100 p-1 sm:p-2 sm:space-y-1" ]
+        [ div [ class "font-bold text-center" ] [ text "Marketplace" ]
+        , div [ class "flex flex-row sm:space-x-1" ]
+            [ div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Lorem" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Ipsum" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Dolor" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "sit" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "amet" ]
+            ]
+        , div [ class "flex flex-row sm:space-x-1" ]
+            [ div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Lorem" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Ipsum" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Dolor" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "sit" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "amet" ]
+            ]
+        , div [ class "flex flex-row sm:space-x-1" ]
+            [ div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Lorem" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Ipsum" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "Dolor" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "sit" ]
+            , div [ class "basis-1/5 text-center p-1 border-gray-300 border-2" ] [ text "amet" ]
+            ]
         ]
 
 
@@ -34,11 +79,27 @@ viewOthers others =
 
 viewOther : PompOthersInventory -> Html a
 viewOther other =
-    div []
-        [ div [] [ text (other.name ++ ": " ++ String.fromInt other.energy ++ " energy, ?? victory points.") ]
-        , text ("Fire (" ++ String.fromInt other.fire ++ ")")
-        , text ("Plant (" ++ String.fromInt other.plant ++ ")")
-        , text ("Water (" ++ String.fromInt other.water ++ ")")
-        , text ("Earth (" ++ String.fromInt other.earth ++ ")")
-        , text ("Chaos (" ++ String.fromInt other.chaos ++ ")")
+    div [ class "m-1 bg-gray-100 p-1 sm:p-2" ]
+        [ div [ class "flex flex-row sm:space-x-1" ]
+            [ div [ class "basis-1/4 text-center p-1" ] [ text "0 Points" ]
+            , div [ class "basis-2/4 text-center p-1 font-bold" ] [ text other.name ]
+            , div [ class "basis-1/4 text-center p-1" ] [ text (String.fromInt other.energy ++ " Energy") ]
+            ]
+        , div [ class "flex flex-row sm:space-x-1" ]
+            [ div [ class "basis-1/5 text-center p-1 bg-red-200 border-red-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt other.fire ++ ") Fire")
+                ]
+            , div [ class "basis-1/5 text-center p-1 bg-green-200 border-green-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt other.plant ++ ") Plant")
+                ]
+            , div [ class "basis-1/5 text-center p-1 bg-blue-200 border-blue-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt other.water ++ ") Water")
+                ]
+            , div [ class "basis-1/5 text-center p-1 bg-amber-200 border-amber-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt other.earth ++ ") Earth")
+                ]
+            , div [ class "basis-1/5 text-center p-1 bg-purple-200 border-purple-500 border-2" ]
+                [ text ("0 (+" ++ String.fromInt other.chaos ++ ") Chaos")
+                ]
+            ]
         ]
